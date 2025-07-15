271979e17a7d62cda33dc1c039afd0e41ae757ba
seolhwayam <susung462@gmail.com>
2025-07-15 11:40:50 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+// require("express");
+import express from "express";
+
+const app = express();
+
+app.listen(8080, () =>{
+    console.log("server running at....");
+    console.log("추가추가");
+    console.log("추가22");
+});
